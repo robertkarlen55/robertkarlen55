37c355878c1f98e2de9e26e9f0550671e187c4c4
robertkarlen55 <robertkarlen55@gmail.com>
2025-04-29 04:18:02 -0700

    Update WIN10-AU-000500.ps1

--- STIGS/WIN10-AU-000500.ps1
+++ STIGS/WIN10-AU-000500.ps1
@@ -25,8 +25,6 @@
     PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
 #>
 
-# YOUR CODE GOES HERE
-
 # Define the Registry path
 $RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
 
