942443594619d00b240ab0e4d0fc4ff27e57efc2
robertkarlen55 <robertkarlen55@gmail.com>
2025-05-06 08:11:07 -0700

    Update WN10-CC-000145.ps1

--- STIGS/WN10-CC-000145.ps1
+++ STIGS/WN10-CC-000145.ps1
@@ -1,1 +1,40 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that users are prompted for a password on resume from sleep (on battery).
 
+.NOTES
+    Author          : Robert Karlen
+    LinkedIn        : www.linkedin.com/in/robert-karlen-910794209
+    GitHub          : https://github.com/robertkarlen55
+    Date Created    : 2025-05-06
+    Last Modified   : 2024-05-06
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000145
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+# Define the registry path and value
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb-100d-47d6-a2d5-f7d2daa51f51"
+$valueName = "DCSettingIndex"
+$valueData = 1
+
+# Check if the registry path exists; create if necessary
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set registry value to require a password on resume from sleep (on battery)
+Set-ItemProperty -Path $regPath -Name $valueName -Value $valueData -Type DWord -Force
+
+Write-Output "Configured system to require password on resume from sleep (on battery)."
