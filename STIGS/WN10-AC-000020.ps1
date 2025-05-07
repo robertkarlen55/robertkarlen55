3a2f95c7ac6249547fe538a51b768c9e89ea86ef
robertkarlen55 <robertkarlen55@gmail.com>
2025-05-07 14:09:48 -0700

    Create WN10-AC-000020.ps1

---
+++ STIGS/WN10-AC-000020.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the password history is configured to 24 passwords remembered.
+
+.NOTES
+    Author          : Robert Karlen
+    LinkedIn        : www.linkedin.com/in/robert-karlen-910794209
+    GitHub          : https://github.com/robertkarlen55
+    Date Created    : 2025-05-07
+    Last Modified   : 2024-05-07
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000020
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#># 
+net accounts /uniquepw:24
