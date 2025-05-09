c9a650621dde50288ec5e322d5026c49050aaf4b
robertkarlen55 <robertkarlen55@gmail.com>
2025-05-09 11:39:20 -0700

    Update WN10-AC-000020.ps1

--- STIGS/WN10-AC-000020.ps1
+++ STIGS/WN10-AC-000020.ps1
@@ -7,7 +7,7 @@
     LinkedIn        : www.linkedin.com/in/robert-karlen-910794209
     GitHub          : https://github.com/robertkarlen55
     Date Created    : 2025-05-07
-    Last Modified   : 2024-05-07
+    Last Modified   : 2025-05-07
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
